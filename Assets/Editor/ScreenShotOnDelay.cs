67653c1323eddc7d8b42ed8d4ce8b4e2156960d3
Anders Antila <anders.antila@gmail.com>
2016-07-05 01:05:38 +0200

    Add script that takes screenshot everytime you press start

---
+++ Assets/Editor/ScreenShotOnDelay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenshotOnDelay : MonoBehaviour
+{
+
+    public void SetTimer(float time, string path)
+    {
+        StartCoroutine(Capture(time, path));
+    }
+
+    IEnumerator Capture(float time, string path)
+    {
+        yield return new WaitForSeconds(time);
+
+        Application.CaptureScreenshot(path);
+
+        GameObject.Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Editor/ScreenShotOnPlay.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+/**
+ * Put me in a folder named "Editor".
+ * Change SCREENSHOT_PATH to modify where screenshots are saved.
+ */
+[InitializeOnLoad]
+public class ScreenshotOnPlay
+{
+    const string SCREENSHOT_PATH = "Assets/Screenshots/Screenshot_";
+    static bool isPlaying = false;
+
+    static ScreenshotOnPlay()
+    {
+        EditorApplication.playmodeStateChanged += OnPlaymodeStateChange;
+    }
+
+    ~ScreenshotOnPlay()
+    {
+        EditorApplication.playmodeStateChanged -= OnPlaymodeStateChange;
+    }
+
+    static void OnPlaymodeStateChange()
+    {
+        Debug.Log("hey");
+        if (EditorApplication.isPlaying)
+        {
+            isPlaying = !isPlaying;
+
+            if (isPlaying)
+            {
+                string path = "";
+                int i = 0;
+
+                if (EditorPrefs.HasKey("CurrentScreenshot"))
+                    i = EditorPrefs.GetInt("CurrentScreenshot");
+
+                path = AssetDatabase.GenerateUniqueAssetPath(SCREENSHOT_PATH + i + ".png");
+
+                EditorPrefs.SetInt("CurrentScreenshot", i);
+
+                // Instantiate a new gameobject that will capture a screenshot then delete itself.
+                // This works around a lack of coroutines in the editor.
+                GameObject go = new GameObject();
+                go.AddComponent<ScreenshotOnDelay>().SetTimer(2f, path);
+                Debug.Log("hey2");
+            }
+        }
+    }
+}
