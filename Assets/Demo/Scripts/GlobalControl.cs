02a023c69b8e064f9d55bbaf77cc93fb19ef7233
Erik Nahkala <erik@nahkala.se>
2016-07-05 22:37:06 +0200

    Now it _should_ work (and hopefully not be even more broken)

---
+++ Assets/Demo/Scripts/GlobalControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalControl : MonoBehaviour {
+    public static GlobalControl Instance;
+
+    public MultiplayerManager globalMultiplayerManager;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
