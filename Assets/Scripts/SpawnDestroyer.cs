34248f37a81cf427db38fca8ba4c947f31ae2105
Anders Antila <anders.antila@gmail.com>
2016-07-08 01:03:33 +0200

    Added item spawner

---
+++ Assets/Scripts/SpawnDestroyer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnDestroyer : MonoBehaviour {
+
+    public void DestroyItem(Collider other)
+    {
+
+        Debug.Log("OnCollisionEnter");
+        /*if (collision.gameObject.tag == "Pickup")
+        {
+            Debug.Log("Destroy");
+            Destroy(collision.gameObject);
+        }*/
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.layer == 8) // Spawned
+        {
+           Debug.Log("Destroy");
+           Destroy(other.gameObject);
+       }
+    }
+}
