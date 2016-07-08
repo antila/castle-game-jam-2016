ae01e01cc430be60faf2b747d9c9fcefb9ada969
Erik Nahkala <erik@nahkala.se>
2016-07-08 02:23:53 +0200

    Trap Mode Final Ship it!

---
+++ Assets/Demo/Scripts/Helpers/ListExtensions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public static class ListExtensions
+{
+
+    public static void Shuffle<T>(this List<T> list)
+    {
+        System.Random rng = new System.Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+
+
+}
