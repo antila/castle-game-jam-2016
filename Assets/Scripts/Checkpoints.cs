34edab23144dd0c394070e6b3e0f19ba3e30c396
Anders Antila <anders.antila@gmail.com>
2016-07-08 19:24:35 +0200

    Code cleanup

--- Assets/Scripts/Checkpoints.cs
+++ Assets/Scripts/Checkpoints.cs
@@ -7,26 +7,18 @@ public class Checkpoints : MonoBehaviour
     public Color activeColor = Color.green; //color when checkpoint is activated
     public float activeColorOpacity = 0.4f; //opacity when checkpoint is activated
 
-    private Color defColor;
     private GameObject[] checkpoints;
-    private Renderer render;
     private AudioSource aSource;
 
     //setup
     void Awake()
     {
-        render = GetComponent<Renderer>();
         aSource = GetComponent<AudioSource>();
         if (tag != "Respawn")
         {
             tag = "Respawn";
-            Debug.LogWarning("'Checkpoint' script attached to object without the 'Respawn' tag, tag has been assigned automatically", transform);
         }
         GetComponent<Collider>().isTrigger = true;
-
-        if (render)
-            defColor = render.material.color;
-        activeColor.a = activeColorOpacity;
     }
 
     //more setup
