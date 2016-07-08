ce7d40b921d2e20d785015aec079bbb2279758ce
Anders Antila <anders.antila@gmail.com>
2016-07-08 18:35:02 +0200

    Add max number of spawned items for spawner

---
+++ Assets/Scripts/SpawnedObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnedObject : MonoBehaviour
+{
+    public Spawner homeSpawn;
+
+    void OnDestroy()
+    {
+        homeSpawn.spawnCount--;
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,6 +9,12 @@ public class Spawner : MonoBehaviour
     public Vector3 spawnRandomJitter = new Vector3(6, 0, 10);
     public  bool isActive = true;
 
+    //maximum allowed number of objects - set in the editor
+    public int maxObjects = 15;
+
+    //number of objects currently spawned
+    public int spawnCount = 0;
+
     void Start()
     {
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
@@ -17,7 +23,7 @@ public class Spawner : MonoBehaviour
 
     void Spawn()
     {
-        if (item != null && isActive)
+        if (item != null && isActive && spawnCount < maxObjects)
         {
             // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
             GameObject spawn = (GameObject)Instantiate(item, transform.position, transform.rotation);
@@ -26,6 +32,10 @@ public class Spawner : MonoBehaviour
 
             Rigidbody r = spawn.GetComponent<Rigidbody>();
             r.AddRelativeForce(force, ForceMode.VelocityChange);
+
+            spawn.AddComponent<SpawnedObject>();
+            spawn.GetComponent<SpawnedObject>().homeSpawn = this;
+            spawnCount++;
         }
         
     }
